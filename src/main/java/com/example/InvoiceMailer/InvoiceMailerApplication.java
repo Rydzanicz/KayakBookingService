ff323500ddf0b57a0c431503630948370c9cf919
Rydzanicz <asddsfsdfsf@gmail.com>
2024-11-19 09:47:02 +0100

    Added starting files and banner

---
+++ src/main/java/com/example/InvoiceMailer/InvoiceMailerApplication.java
@@ -0,0 +1,14 @@
+package com.example.InvoiceMailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceMailerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceMailerApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/example/InvoiceMailer/InvoiceMailerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InvoiceMailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceMailerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
