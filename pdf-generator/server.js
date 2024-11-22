d1d5b97bb0addb1e2ceb21bc98898e253d5b93de
Rydzanicz <asddsfsdfsf@gmail.com>
2024-11-22 12:16:31 +0100

    Fixed generating PDF for list of products

--- pdf-generator/server.js
+++ pdf-generator/server.js
@@ -14,36 +14,6 @@ app.get('/', (req, res) => {
   res.render('form');
 });
 
-app.post('/generate-invoice', async (req, res) => {
-  try {
-    const { buyerName, buyerAddressEmail, buyerAddress, buyerNip, products } = req.body;
-
-    const response = await axios.post(
-      'http://localhost:8080/generate-invoice',
-      { buyerName, buyerAddressEmail, buyerAddress, buyerNip, products },
-      { responseType: 'arraybuffer' }
-    );
-
-    const contentDisposition = response.headers['content-disposition'];
-    const fileName = contentDisposition.split('filename=')[1].replace(/"/g, '');
-
-    const filePath = path.join(__dirname, fileName);
-
-    fs.writeFileSync(filePath, response.data);
-
-    res.download(filePath, fileName, (err) => {
-      if (err) {
-        console.error('Błąd przy pobieraniu pliku:', err);
-      }
-      fs.unlinkSync(filePath);
-    });
-
-  } catch (error) {
-    console.error('Błąd przy komunikacji z backendem:', error);
-    res.status(500).send('Wystąpił błąd przy generowaniu PDF.');
-  }
-});
-
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
